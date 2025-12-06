a4239d4928ca601ca8c3449cdd7521f909710973
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-06 13:22:29 +0100

    test: added HelloIntegrationTest.java

---
+++ Demo1/Demo1/src/test/java/com/example/Demo1/HelloIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.example.Demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloIntegrationTest {
+  @LocalServerPort
+  private int port;
+
+  private RestClient restClient = RestClient.create();
+
+  @Test
+  void helloEndpoint_withName_returnsCustomGreeting() {
+    String url = "http://localhost:" + port + "/hello?name=Rizwan";
+    String body = restClient.get()
+        .uri(url)
+        .retrieve()
+        .body(String.class);
+    assertEquals("Hello Rizwan", body);
+  }
+}
