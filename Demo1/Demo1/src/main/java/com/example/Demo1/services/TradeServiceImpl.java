bc8337f963f1327967f3be292ef811a643ecc5d7
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-06 08:21:24 +0100

    feat: updated TradeServiceImpl.java

--- Demo1/Demo1/src/main/java/com/example/Demo1/services/TradeServiceImpl.java
+++ Demo1/Demo1/src/main/java/com/example/Demo1/services/TradeServiceImpl.java
@@ -4,65 +4,64 @@ import com.example.Demo1.dtos.TradeDto;
 import com.example.Demo1.entities.TradeEntity;
 import com.example.Demo1.exceptions.ResourceNotFoundException;
 import com.example.Demo1.repositories.TradeRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class TradeServiceImpl implements TradeService {
-  @Autowired
-  private TradeRepository tradeRepository;
-
-  @Autowired
-  private ModelMapper modelMapper;
+  private final TradeRepository tradeRepository;
+  private final ModelMapper modelMapper;
 
   @Override
   public TradeDto create(TradeDto tradeDto) {
     TradeEntity tradeEntity = modelMapper.map(tradeDto, TradeEntity.class);
     tradeEntity.setId(null);
     TradeEntity savedTradeEntity = tradeRepository.save(tradeEntity);
-    return modelMapper.map(savedTradeEntity, TradeDto.class);
+    return mapToDto(savedTradeEntity);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public List<TradeDto> getAll() {
     List<TradeEntity> tradeEntities = tradeRepository.findAll();
     return tradeEntities.stream()
-        .map(tradeEntity -> modelMapper.map(tradeEntity, TradeDto.class))
+        .map(this::mapToDto)
         .toList();
   }
 
   @Override
+  @Transactional(readOnly = true)
   public TradeDto getById(Integer id) {
-    TradeEntity tradeEntity = tradeRepository.findById(id).orElseThrow(() ->
-        new ResourceNotFoundException("Trade not found with id: " + id)
-    );
-    return modelMapper.map(tradeEntity, TradeDto.class);
+    TradeEntity existing = getTradeEntity(id);
+    return mapToDto(existing);
   }
 
   @Override
   public TradeDto deleteById(Integer id) {
-    TradeEntity tradeEntity = tradeRepository.findById(id).orElseThrow(() ->
-        new ResourceNotFoundException("Trade not found with id: " + id)
-    );
+    TradeEntity existing = getTradeEntity(id);
     tradeRepository.deleteById(id);
-    return modelMapper.map(tradeEntity, TradeDto.class);
+    return mapToDto(existing);
   }
 
   @Override
   public TradeDto updateById(Integer id, TradeDto tradeDto) {
-    TradeEntity tradeEntity = tradeRepository.findById(id).orElseThrow(() ->
-        new ResourceNotFoundException("Trade not found with id: " + id)
-    );
-    tradeEntity.setType(tradeDto.getType());
-    tradeEntity.setUserId(tradeDto.getUserId());
-    tradeEntity.setSymbol(tradeDto.getSymbol());
-    tradeEntity.setShares(tradeDto.getShares());
-    tradeEntity.setPrice(tradeDto.getPrice());
-    tradeEntity.setTimestamp(tradeDto.getTimestamp());
-    TradeEntity updatedTradeEntity = tradeRepository.save(tradeEntity);
-    return modelMapper.map(updatedTradeEntity, TradeDto.class);
+    TradeEntity existing = getTradeEntity(id);
+    modelMapper.map(tradeDto, existing);
+    TradeEntity updatedTradeEntity = tradeRepository.save(existing);
+    return mapToDto(updatedTradeEntity);
+  }
+
+  private TradeEntity getTradeEntity(Integer id) {
+    return tradeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Trade not found with id: " + id));
+  }
+
+  private TradeDto mapToDto(TradeEntity entity) {
+    return modelMapper.map(entity, TradeDto.class);
   }
 }
